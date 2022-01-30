ce3025a0db19233428a300a68ea024ec7e0cc41e
Roland Shum <roland.shum@digipen.edu>
2022-01-30 15:31:50 -0800

    Cat tile sounds

--- Assets/Scripts/TileScripts/PetCat.cs
+++ Assets/Scripts/TileScripts/PetCat.cs
@@ -10,7 +10,7 @@ public class PetCat : MonoBehaviour
     public LeverUI lever;
     public Slider slider;
     public CursorEvents cursor;
-
+    public Animator handAnimator;
     public void PetPointsUp()
     {
         var val = (100f / 10f) * Time.deltaTime;
@@ -38,6 +38,7 @@ public class PetCat : MonoBehaviour
     {
         DOTween.To(()=>slider.value,(float something)=>slider.value=something, 0, .1f);
         lever.ResetPercentage();
+        handAnimator.SetTrigger("StopPetting");
     }
 
     public void killTweens()
