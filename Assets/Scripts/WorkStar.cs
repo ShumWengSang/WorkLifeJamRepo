b219fe1219432801346859047a5f65cc5ea59705
Alex-Jalonen <71279927+Alex-Jalonen@users.noreply.github.com>
2022-01-29 18:26:42 -0800

    fixed stats using prefab

--- Assets/Scripts/WorkStar.cs
+++ Assets/Scripts/WorkStar.cs
@@ -2,17 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
-[RequireComponent(typeof(Image))]
 public class WorkStar : MonoBehaviour
 {
+    public Transform mask;
+    public Transform fill;
+    public ParticleSystem particles;
+
+    public bool fillOnAwake;
+
+    private void Awake()
+    {
+        if(fillOnAwake)
+        {
+            FilledIn();
+        }
+        else
+        {
+            Missing();
+        }
+    }
+
+    public void FillLevel(float percent)
+    {
+        mask.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 75f*(1f-percent));
+        fill.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
+    }
+
     public void FilledIn()
     {
-        gameObject.GetComponent<Image>().color = Color.white;
+        mask.position = gameObject.transform.position; 
+        fill.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
     }
 
     public void Missing()
     {
-        gameObject.GetComponent<Image>().color = Color.black;
+        mask.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 75f);
+        fill.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
+    }
+
+    public float celebrateTimer = 1f;
+
+    public void FillUpCelebrate()
+    {
+        //Put particle effects here
+        Invoke(nameof(PlayParticles), celebrateTimer-.1f);
+        mask.DOMoveY(gameObject.transform.position.y, celebrateTimer);
+        fill.DOMoveY(gameObject.transform.position.y, celebrateTimer);
+    }
+
+    public void PlayParticles()
+    {
+        particles.Play();
     }
 }
